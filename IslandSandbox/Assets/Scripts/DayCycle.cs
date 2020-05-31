e5d69dff82228527e03c07bb517b80d471c1ecef
Looluh <62610499+Looluh@users.noreply.github.com>
2020-05-30 22:18:45 -0300

    Entrega

---
+++ IslandSandbox/Assets/Scripts/DayCycle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class DayCycle : MonoBehaviour
+{
+    public float daytime;
+    public float lightDegrees = 0;
+    public float timeScale;
+
+    public TimeSpan myTime;
+    public Light sun;
+    public Color fogColor;
+    public Gradient ambientGradient;
+
+    public delegate void MorningCall();
+    public delegate void NightCall();
+
+    public MorningCall myMorningCall;
+    public NightCall myNightCall;
+
+    bool day = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fogColor = RenderSettings.ambientLight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        daytime += Time.deltaTime * timeScale;
+        myTime = TimeSpan.FromSeconds(daytime);
+        lightDegrees = 360 * (daytime / 86400);
+        transform.rotation = Quaternion.Euler(lightDegrees, -30, 0);
+
+        float normalsun = Vector3.Dot(transform.forward, Vector3.down);
+        sun.intensity = normalsun * 1.2f;
+
+        RenderSettings.ambientLight = ambientGradient.Evaluate(normalsun + .4f);
+        RenderSettings.fogColor = fogColor * normalsun;
+
+        if (normalsun > 0)
+        {
+            if (!day)
+            {
+                myMorningCall();
+                day = true;
+            }
+        }
+        else
+        {
+            if (day)
+            {
+                myNightCall();
+                day = false;
+            }
+        }
+    }
+}
---
+++ IslandSandbox/Assets/Scripts/LightPost.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPost : MonoBehaviour
+{
+    public Light lamp;
+    public MeshRenderer lampMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DayCycle dayscript = GameObject.FindObjectOfType<DayCycle>();
+        dayscript.myNightCall += TurnOn;
+        dayscript.myMorningCall += TurnOff;
+    }
+
+    public void TurnOn()
+    {
+        lamp.enabled = true;
+        lampMat.materials[1].EnableKeyword("_EMISSION");
+    }
+
+    public void TurnOff()
+    {
+        lamp.enabled = false;
+        lampMat.materials[1].EnableKeyword("_EMISSION");
+
+    }
+}
